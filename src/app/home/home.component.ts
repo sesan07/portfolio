08a71285102352d88f33f207ec654a6b504e72ed
Samuel Esan <samuel.esan@proton.me>
2022-05-22 14:55:51 -0300

    feat(home): update description

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,14 +15,20 @@ export class HomeComponent implements OnInit {
     headerHeight: number = 100;
     githubLink: string = 'https://github.com/sesan07';
     resumeLink: string = 'https://drive.google.com/file/d/1stGmZ8Y4VM-zbg-MFIGm2MrDHn6oLO7_/view?usp=sharing'
-    description: string = `Hello! I graduated from the University of Prince Edward Island with a bachelor’s degree in computer science. 
-    While in school, I spent too much of my free time developing and 'deploying' personal projects. 
-    I started out by using Unity 3D to make mobile games for Android. 
-    I then moved on to making native Android apps to give myself a new challenge. 
-    When Covid hit, I had too much free time and decided to look into web development with Angular. 
-    I made my first portfolio website and then created a football website which got me my first job! 
-    Looking to learn more while working, I created an audio visualizer app as a side project on weekends, learned a lot of new things. 
-    I am always looking for new challenges and love learning new technologies!`
+    description: string = `
+        Hi. I'm Sam. I write code.
+
+        I started out by making video games using Unity3D and C#.
+        After making some games, I decided to try my hand at making native android apps with Android Studio and Kotlin.
+        For my graduation project I worked on a library report management tool (based on COUNTER5) using Python and PyQT for the GUI.
+
+        Since graduating, I now work in web development of Angular 2 based apps.
+        I work with technologies like NodeJS, Hapi, RAML, Molecular and Oracle for back end development.
+        I also work with the Elasticsearch, Logastash and Kibaba stack for search.
+        My job involves building dockerized micro-services and apps using Docker Compose.
+
+        I enjoy learning new things by working on personal projects whenever I get the chance!
+    `
 
     constructor(private _route: ActivatedRoute, public projectService: ProjectService) {
     }
